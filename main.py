d38bd4405998f2b1d06378a85e8335ca0fafaf6d
Tomos Williams <tllewwilliams@gmail.com>
2017-12-07 12:03:07 +0000

    less errors when ran in command line (THIS IS PYTHON3 ONLY)

--- main.py
+++ main.py
@@ -7,22 +7,22 @@ def main():
     wantinput = True
     print("Beginning Tic Tac Toe 2.0\n**********************\n")
     while (wantinput):
-        name = input("Enter player Name\n")
-        icon = input("Enter player Icon\n")
-        wantinput = input("Add Player? (y/n)\n") == "y"
+        name = str(input("Enter player Name\n"))
+        icon = str(input("Enter player Icon\n"))
+        wantinput = str(input("Add Player? (y/n)\n")) == "y"
         players.append(Player(name, icon))
     width = int(input("Enter board width\n"))
     height = int(input("Enter board height\n"))
     game = XO(width,height)
     while(not winner):
         for player in players:
-            row = int(input("Enter Column to hit!\n"))
+            row = int(input("%s Enter Column to hit!\n"%player.name))
             game.hit(row, player.icon)
             print(game.print_board())
             if game.check_win():
                 print("%s has Won!!!"%player.name)
                 winner = True
-                if input("Play again?(y/n)") == "y":
+                if str(input("Play again?(y/n)")) == "y":
                     main()
 
 
